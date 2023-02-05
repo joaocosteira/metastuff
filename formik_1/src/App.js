7971361c70e3a7cded42bc73ba289f4e51cde92a
João Costeira <joaocurveira13@gmail.com>
2023-02-05 20:47:46 +0000

    formik 1

---
+++ formik_1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Basic from './components/Basic';
+import Basic2 from './components/Basic2';
+
+function App() {
+  return (
+    <div className="App">
+      <Basic/>
+      <Basic2/>
+    </div>
+  );
+}
+
+export default App;
